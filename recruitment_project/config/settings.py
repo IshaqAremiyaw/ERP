81a39d79ec19b9d9db8e282254ba1533814a2a56
Ishaq Aremiyaw <ishaqaremiyaw47@gmail.com>
2026-08-02 01:00:13 +0000

    Fix indentation error in settings.py

--- recruitment_project/config/settings.py
+++ recruitment_project/config/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "test-secret-key-do-not-use-in-production")
 DEBUG = os.environ.get("DEBUG", "False") == "True" 
- ALLOWED_HOSTS = ["erp-zh67.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["erp-zh67.onrender.com", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
